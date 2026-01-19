21852b3ce81719ff2bb2cf20eedce88c7ab11f9e
malyun hared <mankaharet48@gmail.com>
2026-01-19 15:57:23 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+// Create an employee object (needed by the tests)
+let employee = {};
+
+// NON-DESTRUCTIVE: returns a new object
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+// DESTRUCTIVE: modifies the original object
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// NON-DESTRUCTIVE: deletes key from a clone
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+// DESTRUCTIVE: deletes key from original object
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+// Export everything
+module.exports = {
+    employee,
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey
+};
